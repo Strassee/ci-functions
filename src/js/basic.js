4278f76e83c1a41741686700997baa91c4265166
Strasse <jeka_krsk@mail.ru>
2024-07-03 15:52:35 +0700

    ci-functions v.1.0.3 commit

--- src/js/basic.js
+++ src/js/basic.js
@@ -2,7 +2,7 @@ export default function health(hero) {
   if (hero.health > 50) {
     return 'healthy';
   }
-  if (hero.health > 15 && hero.health <= 50) {
+  if (hero.health >= 15 && hero.health <= 50) {
     return 'wounded';
   }
   return 'critical';
